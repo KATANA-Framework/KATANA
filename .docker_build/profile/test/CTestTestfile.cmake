74c4b55d7dd0ac16e33fdc95c7b0c94b9a4746be
VisageDvachevsky <mr.echxo@gmail.com>
2025-11-25 06:19:49 +0300

    chore: add test harnesses and DX helpers

--- .docker_build/profile/test/CTestTestfile.cmake
+++ .docker_build/profile/test/CTestTestfile.cmake
@@ -1,8 +1,8 @@
-# CMake generated Testfile for 
+# CMake generated Testfile for
 # Source directory: /workspace/test
 # Build directory: /workspace/.docker_build/profile/test
-# 
-# This file includes the relevant testing commands required for 
+#
+# This file includes the relevant testing commands required for
 # testing this directory and lists subdirectories to be tested as well.
 add_test([=[unit_tests]=] "/workspace/.docker_build/profile/test/unit_tests")
 set_tests_properties([=[unit_tests]=] PROPERTIES  _BACKTRACE_TRIPLES "/workspace/test/CMakeLists.txt;43;add_test;/workspace/test/CMakeLists.txt;0;")
